1d8af6c2cb76ee0fe6309d14606147f3d4ab7e87
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-26 16:43:06 -0400

    saved book page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,17 +2,18 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
     query Query {
-        me {
-        username
-        email
-        password
-        bookCount
-        savedBooks {
-            description
-            authors
-            link
-            title
+me {
+    username
+    email
+    password
+    bookCount
+    savedBooks {
+        authors
+        description
+        bookId
+        link
+        title
     }
-    }
-    }
+}
+}
 `;
