4b1d65a62b5e9d51eb4688a7287b8e53700af81b
James Pritchard <james.edward.pritchard@gmail.com>
2022-07-26 21:42:15 -0400

    looking nice

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,35 +1,36 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
 import {GET_ME} from '../utils/queries'
 import {REMOVE_BOOK} from '../utils/mutations'
 import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
 
 
 const SavedBooks = () => {
-  const [deleteBook]= useMutation(REMOVE_BOOK)
-  const [fakeState, setFakeState] = useState('false');
+  const [deleteBook]= useMutation(REMOVE_BOOK)  
+  
+
+
+
   let userData;
-  const {loading, error, data} = useQuery(GET_ME);
+  const {loading, error, data} = useQuery(GET_ME, {pollInterval: 100});
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
   if(!loading && !error){
     userData = data.me
-    
-    
   }
   
   
   
   const handleDeleteBook = async (bookId) => {
-    console.log(fakeState)
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-    
     if (!token) {
       return false;
     }
     deleteBook({variables: {bookId: bookId}, token})
+    removeBookId(bookId);
 
   }
 
@@ -55,9 +56,7 @@ const SavedBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() =>{
-                      handleDeleteBook(book.bookId)
-                      setFakeState(Math.random)}}>
+                    <Button className='btn-block btn-danger' onClick={() =>{handleDeleteBook(book.bookId)}}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
